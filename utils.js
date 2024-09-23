a31f6aac6216801ee3a5d455962119441a3dbf22
Jacob-WAC <81675372+Jacob-WAC@users.noreply.github.com>
2024-09-23 16:13:03 -0500

    adsgf

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 // Utility Functions
 
-function createExplosion(x, y) {
+export function createExplosion(x, y) {
     const particles = [];
     for (let i = 0; i < 20; i++) {
         const angle = Math.random() * Math.PI * 2;
@@ -19,7 +19,7 @@ function createExplosion(x, y) {
     });
 }
 
-function getDistance(x1, y1, x2, y2) {
+export function getDistance(x1, y1, x2, y2) {
     let dx = x1 - x2;
     let dy = y1 - y2;
     return Math.sqrt(dx * dx + dy * dy);
